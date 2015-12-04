e7161a9cce3a30f15f39c1531f05dc7944f6ef06
heleleloga <heleleloga>
2015-12-04 17:41:53 -0500

    comenzando

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+<style type="text/css">
+	body{
+
+		background-color: grey;
+	}
+
+
+
+</style>
+
+</head>
+<body>
+<center>
+<h1>BIENVENIDOS A</h1>
+
+<h2>LACTEOS TAME</h2>
+</center>
+
+<h3>Elaboracion de productos lacteos</h3>
+<ul>
+	
+	<li> <a href="varyog.php"> YOGURT </a></li><br>
+	<li><a href="varque.php"> QUESO </a></li><br>
+	<li><a href="varlec.php"> LECHE </a></li><br>
+	<li><a href="varman.php"> MANTEQUILLA </a></li><br>
+	<li><a href="varareq.php"> AREQUIPE </a></li>
+
+</ul>
+
+</body>
+</html>
---
+++ varareq.php
@@ -0,0 +1,45 @@
+<?php
+
+$servidor = "localhost";
+$base = "lacteos";
+$login = "root";
+$pass = "";
+
+$con=mysql_connect($servidor,$login,$pass)
+ or die ("No se puede conectar");
+
+ mysql_select_db($base,$con) or die ("No base datos");
+
+ $consulta = 'SELECT id, Nombre, Peso, Precio, Disponibilidad FROM arequipe';
+
+ $regs = mysql_query($consulta,$con);
+
+if (!$regs) {
+	echo "Error" . mysql_error();
+}
+
+echo '<table border= "2px">';
+	echo '<tr> <td> id </td> <td> Nombre </td> <td> Peso </td> <td> Precio </td> <td> Disponibilidad </td> <td> editar </td>';
+
+ while ($reg = mysql_fetch_array($regs)) {
+?>
+
+<tr>
+	<td><?php echo $reg['id'];?></td>
+	<td><?php echo $reg['Nombre'];?></td>
+	<td><?php echo $reg['Peso'];?></td>
+	<td><?php echo $reg['Precio'];?></td>
+	<td><?php echo $reg['Disponibilidad'];?></td>
+	
+	<td>
+	<a href="usuarios_eliminar.php?codigo=<?php echo $reg['id'];?>">eliminar</a> 
+	<a href="usuarios_eliminar.php">editar</a>
+</td>
+
+</tr>
+ 
+ <?php	
+ }
+
+?>
+ </table>
